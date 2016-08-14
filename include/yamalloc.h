4ff3131827f05c59c2e9c76e4635cade68cf4cf1
Sharukh Hasan <sharukhhasan@hypercryptic.com>
2016-08-14 18:54:16 -0500

    yamalloc header logic - Sharukh Hasan

--- include/yamalloc.h
+++ include/yamalloc.h
@@ -5,7 +5,38 @@
  *
  */
 
-#ifndef ZIPTREE_YAMALLOC_H
-#define ZIPTREE_YAMALLOC_H
+#ifndef YAMALLOC_H_
+#define YAMALLOC_H_
 
-#endif //ZIPTREE_YAMALLOC_H
+#include <stdlib.h>
+#include <stddef.h>
+//#include <malloc.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern __attribute__((constructor)) void yam_init();
+extern __attribute__((destructor)) void yam_exit();
+extern void yam_free(void* chunk);
+extern void* yam_malloc(size_t sz);
+extern void* yam_realloc(void* chunk, size_t sz);
+extern void* yam_calloc(size_t sz, size_t cnt);
+extern void* yam_memalign(size_t alignment, size_t sz);
+extern size_t yam_malloc_size(void* ptr);
+
+#ifdef __YAM_MALLOC
+#define malloc               yam_malloc
+#define realloc              yam_realloc
+#define free                 yam_free
+#define calloc               yam_calloc
+#define memalign             yam_memalign
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* YAMALLOC_H_ */
