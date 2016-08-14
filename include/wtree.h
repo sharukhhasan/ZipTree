5634fdf540f924600d6b4eaa4ee7be9bd48c468d
Sharukh Hasan <sharukhhasan@hypercryptic.com>
2016-08-14 18:55:06 -0500

    added definitions to wtree header - Sharukh Hasan

--- include/wtree.h
+++ include/wtree.h
@@ -5,7 +5,19 @@
  *
  */
 
-#ifndef ZIPTREE_WTREE_H
-#define ZIPTREE_WTREE_H
+#ifndef INCLUDE_WTREE_H_
+#define INCLUDE_WTREE_H_
 
-#endif //ZIPTREE_WTREE_H
+
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "cdsl_slist.h"
+
+#define DECLARE_WTREE_TRAVERSE_CALLBACK(fn)             BOOL  fn(wtreeNode_t* node,void* arg)
+
+#define DECLARE_ONALLOCATE(fn)                              void* fn(size_t total_sz, size_t* rsz, void* ext_ctx)
+#define DECLARE_ONFREE(fn)                                  int fn(void* addr, size_t sz, wtreeNode_t* wtnode, void* ext_ctx)
+#define DECLARE_ONREMOVED(fn)                                       void fn(wtreeNode_t* node, void* ext_ctx)
+#define DECLARE_ONADDED(fn)                                         void fn(wtreeNode_t* node, void* ext_ctx)
