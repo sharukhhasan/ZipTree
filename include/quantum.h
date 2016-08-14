6f28c09ba050aa650f073f12779fbf62d363cc2f
Sharukh Hasan <sharukhhasan@hypercryptic.com>
2016-08-14 18:52:21 -0500

    quantum header complete - Sharukh Hasan

--- include/quantum.h
+++ include/quantum.h
@@ -5,7 +5,45 @@
  *
  */
 
-#ifndef ZIPTREE_QUANTUM_H
-#define ZIPTREE_QUANTUM_H
+#ifndef INCLUDE_QUANTUM_H_
+#define INCLUDE_QUANTUM_H_
 
-#endif //ZIPTREE_QUANTUM_H
+
+#include "wtree.h"
+#include "cdsl_nrbtree.h"
+#include "cdsl_slist.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define QUANTUM_SPACE           ((uint16_t) 2)
+#define QUANTUM_MAX             ((uint16_t) 32)
+#define QUANTUM_COUNT_MAX		((uint16_t) 2048)
+
+
+typedef struct {
+    nrbtreeRoot_t    addr_rbroot;
+    nrbtreeRoot_t    quantum_tree;
+    wtreeRoot_t      quantum_pool;
+    slistEntry_t     clr_lentry;
+    wt_adapter       adapter;
+}quantumRoot_t;
+
+extern void quantum_root_init(quantumRoot_t* root,wt_map_func_t mapper, wt_unmap_func_t unmapper);
+extern void* quantum_reclaim_chunk(quantumRoot_t* root, ssize_t sz);
+extern size_t quantum_get_chunk_size(quantumRoot_t* root, void* chunk);
+extern int quantum_free_chunk(quantumRoot_t* root,void* chunk);
+extern void quantum_try_purge_cache(quantumRoot_t* root);
+extern void quantum_cleanup(quantumRoot_t* root);
+extern void quantum_print(quantumRoot_t* root);
+extern size_t quantum_size(quantumRoot_t* root, void* chunk);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* INCLUDE_QUANTUM_H_ */
